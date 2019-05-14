5a9825688ebbeaaabdaec8f83ee17465d78fb465
Zakcrust <zakcrusted@gmail.com>
2019-05-14 16:51:28 +0700

    First Commit Sudoku

---
+++ app/src/main/java/com/zak/remindly/DateTimeSorter.java
@@ -0,0 +1,23 @@
+package com.zak.remindly;
+
+public class DateTimeSorter {
+    public int mIndex;
+    public String mDateTime;
+    public DateTimeSorter(int index, String DateTime){
+        mIndex = index;
+        mDateTime = DateTime;
+    }
+    public DateTimeSorter(){}
+    public int getIndex() {
+        return mIndex;
+    }
+    public void setIndex(int index) {
+        mIndex = index;
+    }
+    public String getDateTime() {
+        return mDateTime;
+    }
+    public void setDateTime(String dateTime) {
+        mDateTime = dateTime;
+    }
+}
